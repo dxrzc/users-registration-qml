d58c41cbec9ecb1a2385e355139ca04b8a2100b5
Diego Rodriguez <anexomailer@gmail.com>
2024-04-28 16:11:08 -0500

    add: userTableModel class

---
+++ interface/table/userTableModel.cpp
@@ -0,0 +1,77 @@
+#include "userTableModel.h"
+
+UserTableModel::UserTableModel(qmlDto* dto_, QObject* parent): QAbstractTableModel(parent),dto(dto_)
+{
+    filterON = false;
+    dto->getAllUsers(usersList);
+    QObject::connect(dto,&qmlDto::userCreated,this,&UserTableModel::createUser);
+    QObject::connect(dto,&qmlDto::filterEnabled,this,&UserTableModel::enableFilter);
+    QObject::connect(dto,&qmlDto::filterDisabled,this,&UserTableModel::disableFilter);
+}
+
+Q_INVOKABLE int UserTableModel::rowCount(const QModelIndex& parent) const
+{    
+    return (filterON) ? qmlList.count() : usersList.count();
+}
+
+Q_INVOKABLE int UserTableModel::columnCount(const QModelIndex& parent) const
+{
+    return 4;
+}
+
+Q_INVOKABLE QVariant UserTableModel::data(const QModelIndex& index, int role) const
+{
+
+    auto display = [&] <typename T>  (const T& list, const User& currentUser) -> QVariant
+    {
+        if (!index.isValid() || index.row() >= list.count() || index.row() < 0)
+            return QVariant();
+
+        switch (role)
+        {
+        case Qt::DisplayRole:
+            switch (index.column())
+            {
+            case 0: return currentUser.username();
+            case 1: return currentUser.email();
+            case 2: return currentUser.phoneNumber();
+            case 3: return currentUser.birthdate().toQString();
+            }
+        }
+
+        return QVariant();
+    };
+
+    if(filterON)
+        return display(qmlList,qmlList[index.row()].get());
+    else
+        return display(usersList,usersList[index.row()]);
+}
+
+void UserTableModel::createUser(const User& user)
+{
+    beginInsertRows(QModelIndex(), usersList.count(), usersList.count());
+    usersList.append(user);
+    endInsertRows();
+}
+
+void UserTableModel::enableFilter(const QString &filter)
+{
+    qmlList.clear();
+    filterON = true;
+
+    for(const User& user: usersList)
+        if(user.username().startsWith(filter))
+            qmlList.push_back(std::ref(user));
+}
+
+void UserTableModel::disableFilter()
+{    
+    filterON = false;
+}
+
+UserTableModel::~UserTableModel()
+{
+    delete dto;
+}
+
---
+++ interface/table/userTableModel.h
@@ -0,0 +1,35 @@
+#ifndef USERTABLEMODEL_H
+#define USERTABLEMODEL_H
+
+#include <qqml.h>
+#include <QAbstractTableModel>
+#include <QList>
+#include "../../domain/dtos/qmldto.h"
+
+class UserTableModel : public QAbstractTableModel
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_ADDED_IN_VERSION(1, 1)
+
+private:
+    qmlDto* dto;
+    QList<User> usersList;
+    QList<std::reference_wrapper<const User>> qmlList;
+    bool filterON;
+
+public:
+    UserTableModel(qmlDto* , QObject* parent = nullptr);
+    Q_INVOKABLE int rowCount(const QModelIndex& parent) const override;
+    Q_INVOKABLE int columnCount(const QModelIndex& parent) const override;
+    Q_INVOKABLE QVariant data(const QModelIndex& index, int role) const override;
+
+    ~UserTableModel();
+
+public slots:
+    void createUser(const User&);
+    void enableFilter(const QString& filter);
+    void disableFilter();
+};
+
+#endif // USERTABLEMODEL_H
