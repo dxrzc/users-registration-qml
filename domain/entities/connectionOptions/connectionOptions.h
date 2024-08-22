0b6fc4bd6a7e2f31ef348e7248ebcdc49b19ee4b
Diego Rodriguez <anexomailer@gmail.com>
2024-08-22 18:23:07 -0500

    add: connectionOptions class equality operator

--- domain/entities/connectionOptions/connectionOptions.h
+++ domain/entities/connectionOptions/connectionOptions.h
@@ -14,6 +14,11 @@ struct ConnectionOptions
     ConnectionOptions(const QString& hostname, const QString& user, const QString& password
                       , const QString& database, quint64 PORT) : hostname(hostname), user(user), password(password),
         database(database), PORT(PORT) {}
+
+    bool operator== (const ConnectionOptions& co) const noexcept
+    {
+        return (hostname == co.hostname && user == co.user && password == co.password && database == co.database && PORT == co.PORT);
+    }
 };
 
 
